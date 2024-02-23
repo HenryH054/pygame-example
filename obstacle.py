f281d8e27ef7cb3ebfe1ebec965f4cfcc0555678
HenryH054 <henryphutchinson@gmail.com>
2024-02-23 11:44:37 -0700

    edited notes

--- obstacle.py
+++ obstacle.py
@@ -1,4 +1,5 @@
 import pygame
 
-# Add code that generates pipes, moves them by a passed in value and returns the "safe zone"
+# Add code that generates pipes
+# moves them by a passed in value and returns the "safe zone"
 # Make code such that the obstacles can be stored in an array as they move
